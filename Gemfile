f2f0091e0f3a8c887d6e962a584d041089b339ba
Gabriel Gilder <ggilder@tractionco.com>
2011-09-21 01:52:34 -0700

    sass and sinatra assetpack

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "http://rubygems.org"
 
-gem "sinatra", "~> 1.2.6"
+gem "sinatra", "~> 1.2.6", :require => 'sinatra/base'
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
+gem "sass", "~> 3.1.7"
 
 group :development do
   gem "rspec", "~> 2.6.0"
--- app/adjective_generator.rb
+++ app/adjective_generator.rb
@@ -1,10 +1,21 @@
-require 'sinatra/base'
+require 'bundler/setup'
+Bundler.require(:default)
 
 Dir.glob(File.join(File.dirname(__FILE__), 'adjective_generator', '*.rb')).each { |f| require f }
 
 module AdjectiveGenerator
   class App < Sinatra::Base
     set :root, File.dirname(__FILE__)
+    register Sinatra::AssetPack
+
+    assets {
+      serve '/css', from: 'stylesheets'
+      css_compression :sass
+      css :application, '/css/application.css', [
+        '/css/styles.css'
+      ]
+    }
+
     get '/' do
       @word = AdjectiveGenerator::Adjective.random
       erb :index
